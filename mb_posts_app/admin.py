e4182a653a9f5d375d1220560fa02fe216bb8998
Art Ghost <art@ghost>
2020-05-14 15:00:17 -0500

    After adding to ADMIN:  update mb_posts_app/admin.py

--- mb_posts_app/admin.py
+++ mb_posts_app/admin.py
@@ -1,3 +1,10 @@
+# DFB38_04_mesg_board/mb_posts_app/admin.py
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import MbPost
+
+admin.site.register(MbPost)
+
+
+### end ###
