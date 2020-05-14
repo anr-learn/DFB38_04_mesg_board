c8d2c913898192ded220f943b3271d5ca560a528
Art Ghost <art@ghost>
2020-05-14 15:06:29 -0500

    Add __str__ to model class MbPost

--- mb_posts_app/models.py
+++ mb_posts_app/models.py
@@ -8,6 +8,10 @@ class MbPost(models.Model):
 	# 'text' in the book
 	mbPostText = models.TextField()
 
+	def __str__(self):
+		return ("%s: %s" % 
+			(self.__class__.__name__, self.mbPostText[:50]))
+
 
 
 ### end ###
